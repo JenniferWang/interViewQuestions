50ea20afbde703abea90c3c90c96ae8a0ec15c73
Jennifer Jiyue Wang <jiyue@stanford.edu>
2015-01-11 21:08:43 -0800

    Yunzhi's

--- Equal.py
+++ Equal.py
@@ -1,35 +1,32 @@
 import sys
 class Candy():
-  def __init__(self):
-    self.findMinSteps(1000)
+  def __init__(self, initial):
+    self._initial = initial
 
-  def findMinSteps(self, n):
-    self.minSteps = [0]
-    for i in range(1, n + 1):
-      candidates = [self.minSteps[i - 1]]
-      if i - 2 > -1:
-        candidates.append(self.minSteps[i - 2])
-      if i - 5 > -1:
-        candidates.append(self.minSteps[i - 5])
-      self.minSteps.append(min(candidates) + 1)
+  def _findMinSteps(self, n):
+    n1 = n % 5
+    n2 = n1 % 2
+    return n / 5 + n1 / 2 + n2
 
-  def findMinOperation(self, initial):
-    min_candy = min(initial)
-    count = 0
-    for candy in initial:
-      if candy - min_candy > 0:
-        count += self.minSteps[candy - min_candy]
-    return count
+  def _findMinOperationToResult(self, result):
+    return sum([self._findMinSteps(num - result) for num in self._initial])
+
+  def findMinOperation(self):
+    min_candy = min(self._initial)
+    operations = [
+      self._findMinOperationToResult(result) 
+      for result in xrange(min_candy - 4, min_candy + 1)
+    ]
+    return min(operations)
 
 def main():
-  g = sys.stdin
-  #g = open('EQUAL')
+  # g = sys.stdin
+  g = open('EQUAL')
   num_tests = int(g.readline())
-  candy = Candy()
   for _ in range(num_tests):
     g.readline()
     initial = map(int, g.readline().split())
-    print candy.findMinOperation(initial)
+    print Candy(initial).findMinOperation()
 
 if __name__ == '__main__':
   main()
